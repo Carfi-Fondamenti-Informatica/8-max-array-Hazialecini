ace12cfed49b6e2bd7f96373a3b2fdc90d881dc6
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-12-11 14:36:16 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by hazia lecini on 11/12/21.
+//
 
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+float numeroMassimo(const float numeri[],int dimensione);
+#endif //ESERCITAZIONE_8_LIB_H
