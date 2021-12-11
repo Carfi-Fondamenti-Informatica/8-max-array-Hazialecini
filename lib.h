3cd97938f7602e7345cdf6856bde0212e19d237e
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-12-11 14:48:37 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -4,5 +4,5 @@
 
 #ifndef ESERCITAZIONE_8_LIB_H
 #define ESERCITAZIONE_8_LIB_H
-float numeroMassimo(const float numeri[],int dimensione);
+float numeroMassimo(const float numeri[],int dimensione);;
 #endif //ESERCITAZIONE_8_LIB_H
