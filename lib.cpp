0fb2763afd7944eb867d76a08c65b6346c1c43ee
Hazialecini <91554788+Hazialecini@users.noreply.github.com>
2021-12-11 14:36:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by hazia lecini on 11/12/21.
+//
+#include "lib.h"
+
+float numeroMassimo(const float numeri[], int dim) {
+    numeri[dim];
+    float massimo = numeri[0];
+    for (int i = 0; i < dim; i++) {
+        massimo = numeri[i];
+    }
+}
+
+return massimo;
+
+
+
+
 
